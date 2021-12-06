e72a1ee022e93e32007edebba75ab6164ab485d6
oscarlee <oscarlee@mtech-systems.com>
2021-12-06 14:35:12 -0500

    Add project files.

---
+++ Common/FileReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Common
+{
+    public class FileReader
+    {
+        public static string[] LinesAsString(string filename)
+        {
+            var lines = new List<string>();
+
+            var reader = new StreamReader($"{filename}");
+
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                lines.Add(line);
+            }
+
+            return lines.ToArray();
+        }
+
+        public static int[] LinesAsInt(string filename)
+        {
+            return LinesAsString(filename).Select(int.Parse).ToArray();
+        }
+    }
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,46 @@
+﻿using Common;
+using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = FileReader.LinesAsInt(@"input.txt");
+            Console.WriteLine("\n\n\n#################\n");
+            Part1(list);
+            Console.WriteLine("\n");
+            Part2(list);
+            Console.WriteLine("\n#################\n\n\n");
+
+        }
+
+        private static void Part1(int[] list)
+        {
+            var previous = -1;
+            var count = -1;
+            foreach (var item in list)
+            {
+                if (item > previous) count++;
+                previous = item;
+            }
+
+            Console.WriteLine($"Number of depth measurment increases = {count}");
+        }
+
+        private static void Part2(int[] list)
+        {
+            var previous = -1;
+            var count = -1;
+            for (var i = 0; i < list.Length - 2; i++)
+            {
+                var sum = list[i] + list[i + 1] + list[i + 2];
+                if (sum > previous) count++;
+                previous = sum;
+            }
+
+            Console.WriteLine($"Number of multi-depth measurment increases = {count}");
+        }
+    }
+}
